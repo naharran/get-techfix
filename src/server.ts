6b35de39d688c78b072087b9de3ebd63c28dcaf5
Nahar Ran <nahar1995@gmail.com>
2023-12-23 20:54:59 +0200

    try with additional cors middleware

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,11 @@ dotenv.config();
 
 const app = express();
 app.use(cors())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
